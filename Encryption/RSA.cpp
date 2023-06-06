a7595a348a4039934eeb4647de91b519737fe5e0
JoKer <iamamandeep2002123@gmail.com>
2023-06-06 22:38:43 +0530

    cmake integration

--- Encryption/RSA.cpp
+++ Encryption/RSA.cpp
@@ -1,48 +1,66 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class RSA{
-    int n1,n2;
+class RSA
+{
+    long long int n1, n2;
+    long long int pkey;
 
-    public: 
-    //prime numbers for the main stuff
-    void getprimes( int a , int b){
+public:
+    // prime numbers for the main stuff
+    void getprimes(long long int a, long long int b)
+    {
         n1 = a;
         n2 = b;
     }
 
-    //phi fucntion value
-    int phi(){
-        return (n1-1)*(n2-1);
+    // phi function value
+    long long int phi()
+    {
+        return (n1 - 1) * (n2 - 1);
     }
 
-    //passing two integers in the function to declare the public keys
-    void publickeys(int *pkey1, int *pkey2){
-        *pkey1 = n1*n2;
-        *pkey2 = (phi()/2)-1;
+    // passing two integers in the function to declare the public keys
+    void publickeys(long long int *pkey1, long long int *pkey2)
+    {
+        *pkey1 = n1 * n2;
+        *pkey2 = (phi() / 2) - 1;
+        pkey = *pkey2;
     }
 
-    void privatekeys(int *pkey1, int *pkey2){
-        
-
+    void privatekeys(long long int *pkey1)
+    {
+        for (long long int i = 1;; i++)
+        {
+            long long int d = (i * phi() + 1);
+            if (d % pkey == 0)
+            {
+                *pkey1 = d*pkey;
+                cout << i << endl;
+                break;
+            }
+        }
     }
+
+
 };
 
-
-int main(){
-    long long int prime1,prime2;
-    cout<<"Enter two prime numbers: ";
-    cin>>prime1;
-    cin>>prime2;
+int main()
+{
+    long long int prime1, prime2;
+    cout << "Enter two prime numbers: ";
+    cin >> prime1;
+    cin >> prime2;
 
     RSA rsa;
-    int pblc1,pblc2;
+    long long int pblc1, pblc2, prvt;
 
     rsa.getprimes(prime1, prime2);
-    cout<<rsa.phi()<<endl;
+    cout << "phi value " << rsa.phi() << endl;
     rsa.publickeys(&pblc1, &pblc2);
-    cout<<pblc1<<endl;
-    cout<<pblc2<<endl;
-    
+    cout << "public key1 " << pblc1 << endl;
+    cout << "public key2 " << pblc2 << endl;
 
+    rsa.privatekeys(&prvt);
+    cout<<"private key "<< prvt<< endl;
 }
---
+++ build/CMakeFiles/cmake_build.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_build.dir/Encryption/RSA.cpp.o"
+  "CMakeFiles/cmake_build.dir/Encryption/RSA.cpp.o.d"
+  "cmake_build"
+  "cmake_build.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_build.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
